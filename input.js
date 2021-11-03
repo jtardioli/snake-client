9bb4a7225c32b4311f870db4d29c09eff42959d5
jtardioli <joshua.tardioli@gmail.com>
2021-11-03 17:20:34 -0400

    complete

--- input.js
+++ input.js
@@ -1,17 +1,43 @@
+let connection;
 const handleUserInput = function() {
   process.stdin.on('data', (key) => {
-    // \u0003 maps to ctrl+c input
+    process.setMaxListeners(Infinity);
+    
     if (key === '\u0003') {
       process.exit();
+    }
+    if (key === 'w') {
+      
+      connection.write("Move: up");
+    }
+    if (key === 'a') {
+      connection.write("Move: left");
+    }
+    if (key === 's') {
+      connection.write("Move: down");
+    }
+    if (key === 'd') {
+      connection.write("Move: right");
     }
   });
 };
 
-const setupInput = function() {
+// w = "Move: up"
+
+// a = "Move: left"
+
+// s = "Move: down"
+
+// d = "Move: right"
+
+
+
+const setupInput = function(conn) {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
+  connection = conn;
   stdin.on("data", handleUserInput);
   return stdin;
 };
