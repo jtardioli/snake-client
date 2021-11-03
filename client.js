b94b2605643602fa78175ffcc4680ba756aa65a4
jtardioli <joshua.tardioli@gmail.com>
2021-11-03 15:33:24 -0400

    test moving

--- client.js
+++ client.js
@@ -1,15 +1,20 @@
 const net = require("net");
 
 const connect = function() {
+  // create server
   const conn = net.createConnection({
     host: '165.227.47.243',
     port: 50541
   });
-
   conn.setEncoding("utf8");
+
+  // On connect
   conn.on("connect", () => {
     console.log('Connected!!!');
+    conn.write("Name: JDT");
   });
+
+  // recieve data from server
   conn.on("data", (data) => {
     console.log(data);
   });
